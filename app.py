326e99a92c0b9954b86639e1d6cf3e90799289af
anberns <anberns@gmail.com>
2018-07-14 10:23:36 -0400

    uuid-generated user id stored as cookie, used to load previous crawls

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+import uuid
+from flask import Flask, request, render_template, make_response 
 from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://heroku_zlgnt8hx:8qj45t037p6on1oj0r472epmhq@ds233551.mlab.com:33551/heroku_zlgnt8hx"
@@ -7,12 +8,24 @@ mongo = PyMongo(app)
 #index page with form
 @app.route('/')
 def index():
-	return render_template('index.html')
+	userId = request.cookies.get('userId')
+
+	if userId:
+		#get stored data
+		test = mongo.db.test #access test collection
+		storedCrawls = test.find({'userId' : userId})
+		return render_template('index.html', crawls=storedCrawls)
+	else:
+		userId = str(uuid.uuid4())
+		resp = make_response(render_template('index.html'))
+		resp.set_cookie('userId', userId)
+		return resp
 
 #to be used to call webcrawler with posted data
 @app.route('/submit', methods=['POST'])
 def crawl():
 
+	userId = request.cookies.get('userId')
 	url = request.form['url']
 	limit = request.form['limit']
 	sType = request.form['type']
@@ -28,7 +41,7 @@ def crawl():
 
 	#store search in database
 	test = mongo.db.test #access test collection
-	postid = test.insert({'url': url, 'limit': limit, 'sType' : sType, 'keyword' : keyword, 'path' : crawlData.get('path'), 'found' : crawlData.get('found')}) 
+	postid = test.insert({'userId' : userId, 'url': url, 'limit': limit, 'sType' : sType, 'keyword' : keyword, 'path' : crawlData.get('path'), 'found' : crawlData.get('found')}) 
 
 	#get data from id
 	queryData = test.find_one({'_id' : postid})
