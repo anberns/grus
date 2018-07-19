c31fe7a4030ca8c5bd97f16546e2e7f89623cdc4
hschallh <hschallh@gmail.com>
2018-07-19 12:18:29 -0400

    Test getting data from the server

--- app.py
+++ app.py
@@ -32,7 +32,48 @@ def index():
 
 @app.route('/chart', methods=['GET'])
 def chart():
-    return render_template('show_data.html')
+    data = {
+        'a': {
+            'parent': None,
+            'url': "test.com",
+            'depth': 0,
+            'title': "Test",
+            'links': [
+                "test.com/page1"
+            ]
+        },
+        'b': {
+            'parent': "test.com",
+            'url': "test.com/page1",
+            'depth': 1,
+            'title': "Page 1",
+            'links': [
+                "test.com/page2",
+                "test.com/page3",
+            ]
+        },
+        'c': {
+            'parent': "test.com/page1",
+            'url': "test.com/page2",
+            'depth': 2,
+            'title': "Page 2",
+            'links': [
+                "test.com/page4",
+                "test.com/page5",
+            ]
+        },
+        'd': {
+            'parent': "test.com/page2",
+            'url': "test.com/page4",
+            'depth': 3,
+            'title': "Page 4",
+            'links': [
+                "test.com/page6",
+                "test.com/page7",
+            ]
+        }
+    }
+    return render_template('show_data.html', data=data)
 
 # to be used to call webcrawler with posted data
 
