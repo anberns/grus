f0608ef02fb64d448831c1a0f2b477dd037a28cd
anberns <anberns@gmail.com>
2018-07-26 19:09:08 -0400

    removed wait from fork

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def launch():
 
 	#new process for crawler
 	if not os.fork():
-		time.sleep(.5)
+		#time.sleep(.5)
 		return redirect('/crawl')
 
 	return render_template('show_data.html', data=None, url=url, keyword=keyword, type=sType)
