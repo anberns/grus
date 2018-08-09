4cf7696d3500cd7d37a4073abebb5332185bbb30
bonnerj <32306277+bonnerj@users.noreply.github.com>
2018-08-09 13:20:38 -0400

    Revert "Moved check to robots.txt to keep app from idling"
    This reverts commit 954e14592d27a043ed5e0fb4b5b3f9e0ac7fc9e0.

--- crawler.py
+++ crawler.py
@@ -1,3 +1,9 @@
+#pip install BeautifulSoup4
+#pip install validators
+#pipenv install requests
+#pip install lxml
+
+
 # References:
 #	https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
 #	BEAUTIFUL SOUP: used to parse the page for data (urls)
@@ -6,8 +12,8 @@
 #	VALIDATORS: used to validate the urls found
 #	http://validators.readthedocs.io/en/latest/#
 
-#	Queue module: states "especially useful in threaded programming when information must 
-#	be exchanged safely between multiple threads"
+#	Queue module: Thinking ahead...states "especially useful in
+#	threaded programming when information must be exchanged safely between multiple threads"
 
 
 from urllib.parse import urljoin
@@ -29,10 +35,7 @@ import urllib.robotparser
 
 class Spider(object):
 	
-	#Constructor
 	def __init__(self, URL, limit, keyword=None):
-		if not URL.endswith('/'):
-			URL = URL + '/'
 		self.start = URL
 		self.limit = limit
 		if keyword:
@@ -43,50 +46,36 @@ class Spider(object):
 		self.rulesDict = {}
 		self.noRules = []
 
-	#Checks the robots.txt to see if crawls are allowed
 	def checkRbTXT(self, URL):
-		#parses the url to find the base url
 		parsed_uri = urlparse(URL)
 		base = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
 
-		if URL == self.start:
-			return True
-
-		#checks if rules were already checked and found missing/unreadable
 		if base in self.noRules:
 			return True
 
-		#if rules have not already been parsed for that site
 		if base not in self.rulesDict:
 			#print("Robots.txt link: ", rbtxt)
-
-			#create a robotparser and set the url
 			rules = urllib.robotparser.RobotFileParser()
 			rules.set_url(urljoin(base,'robots.txt'))	
 	
 			try:
-				#read the rules
-				#print("Fetching rules for:", base)
+				print("Fetching rules for:", base)
 				rules.read()
 
 			except:
-				#if rules do not exist or cannot be read
-				#print("Cannot fetch rules from:", base)
+				print("Cannot fetch rules from:", base)
 				self.noRules.append(base)
 				return True
 			
 			else:
-				#add to dictionary of robots
 				self.rulesDict[base] = rules
 		
 		if not self.rulesDict[base].can_fetch("*", URL):
 			print (URL, "was NOT INCLUDED per robots.txt")
 	
-		#return whether the site allows crawls for that URL
 		return self.rulesDict[base].can_fetch("*", URL)
 			
 
-	#checks the link to see if it is a media file
 	def checkMedia(self, URL):
 		#print("Checking for media...")
 		toIgnore = ['mp4', 'mp3', 'mov', 'flv', 'wmv', 'avi', 'png', 'gif', 'jpg', 'bmp']
@@ -96,42 +85,35 @@ class Spider(object):
 		return True
 
 	
-	#parses the page to find the HTML content
 	def parsePage(self, URL):
+		try:  #attempts to load page first
+			response = requests.get(URL, timeout=5)
+			response.raise_for_status()
 
-		if not self.checkRbTXT(URL):
+		#returns None upon any page loading error
+		except requests.exceptions.HTTPError:
+			print("Error in retrieving URL")
+			return None
+		except requests.exceptions.SSLError:
+			print("Error in SSL certificate")
+			return None
+		except requests.exceptions.Timeout:
+			print("Error in retrieving URL due to Timeout")
+			return None
+		except requests.exceptions.TooManyRedirects:
+			print("Error in retrieving URL due to redirects")
+			return None
+		except requests.exceptions.RequestException:
+			print("Error in retrieving URL")
 			return None
 
+		#or returns soup
 		else:
-			try:  #attempts to load page first
-				response = requests.get(URL, timeout=1)
-				response.raise_for_status()
-
-			#returns None upon any page loading error
-			except requests.exceptions.HTTPError:
-				print("Error in retrieving URL")
-				return None
-			except requests.exceptions.SSLError:
-				print("Error in SSL certificate")
-				return None
-			except requests.exceptions.Timeout:
-				print("Error in retrieving URL due to Timeout")
-				return None
-			except requests.exceptions.TooManyRedirects:
-				print("Error in retrieving URL due to redirects")
-				return None
-			except requests.exceptions.RequestException:
-				print("Error in retrieving URL")
-				return None
-
-			#or returns soup
-			else:
-				myPage = response.content
-				soup = BeautifulSoup(myPage, 'lxml')
-				#print("Getting Soup")
-				return soup
+			myPage = response.content
+			soup = BeautifulSoup(myPage, 'lxml')
+			#print("Getting Soup")
+			return soup
 			
-	#formats the url to avoid repeated links and to create absolute urls
 	def formatURL(self, base, url):
 		#handles relative URLs - by looking for links lacking http and
 		#joining these to the base URL to form an absolute URL
@@ -143,36 +125,39 @@ class Spider(object):
 		url = removeQuery[0]
 		
 		#strips off trailing forward slash
-		if not url.endswith('/'):
-			url = url + '/'
+		if url.endswith('/'):
+			url = url[:-1]
 
 		return url
 
-	#finds and returns the page titles
 	def findPageTitle(self, soup):
 		#finds the first title tag
 		title = soup.title
 		if title is not None:
 			return title.get_text()
+'''
+	def printVisited(self):
+		print("Visited List: ")
+		print((json.dumps(self.visited, indent=4)))
 
+	def getVisited(self):
+		return self.visited
+'''
 
 
 class BFS(Spider):
 
-	#constructor
 	def __init__ (self, URL, limit, keyword=None):
 		self.URL_list = queue.Queue()
 		#inherit Spider constructor
 		super(BFS, self).__init__(URL, limit, keyword)
 
-	#finds and returns links on page
 	def findConnections(self, base, parent, soup):
 		connections= []
 		#find_all looks for all links on the page
 		for link in soup.find_all('a', href=True):
 			url = link['href']
 
-			#formats the url
 			url = self.formatURL(base, url)
 
 			#verifies link found is valid url and not a duplicate and not the same as the parent url
@@ -181,7 +166,6 @@ class BFS(Spider):
 
 		return connections
 
-	#completes the breadth first search
 	def search(self, ws, postid, database):
 
 		global endEarly
@@ -203,14 +187,12 @@ class BFS(Spider):
 		#while the depth of visited pages is less than the user-set limit
 		while not keywordFound and not self.URL_list.empty() and not endEarly:
 
-			#saves off data
 			parent = self.URL_list.get()
 			currentURL = parent.get('url')
 			depth = parent.get('depth')
 			myParent = parent.get('parent')
-			#currentURL.rstrip('/')
+			currentURL.rstrip('/')
 
-			#checks if limit has been reached
 			if depth > self.limit:
 				break
 
@@ -228,7 +210,6 @@ class BFS(Spider):
 					link_info['parent'] = myParent
 					link_info['found'] = False
 
-					#checks for keyword if one was entered
 					if (self.keyword != None) and soup.find_all(string=re.compile(r'\b%s\b' % self.keyword, re.IGNORECASE)):
 							keywordFound = True
 							link_info['found'] = True
@@ -243,7 +224,6 @@ class BFS(Spider):
 							except Exception as e:
 								print(str(e))
 
-					#adds to visited list
 					self.visited[currentURL] = True
 
 					# send info to visualizer
@@ -262,10 +242,10 @@ class BFS(Spider):
 
 					#all children must be put into the queue as URL_list for next iteration
 					for link in link_info['links']:
-						if self.checkMedia(link) and link not in self.visited: #and self.checkRbTXT(link):
+						if self.checkMedia(link) and self.checkRbTXT(link) and link not in self.visited:
 							parentinfo = {}
 							parentinfo['url'] = link
-							parentinfo['depth'] = depth + 1 #update depth
+							parentinfo['depth'] = depth + 1
 							parentinfo['parent'] = currentURL
 							self.URL_list.put(parentinfo)
 
@@ -273,48 +253,40 @@ class BFS(Spider):
 
 class DFS(Spider):
 
-	#constructor
 	def __init__ (self, URL, limit, keyword=None):
 		self.URL_list = []
 		super(DFS, self).__init__(URL, limit, keyword)
 
-	#finds links on parent page
 	def findConnections(self, base, parent, soup):
 		#find_all looks for all links on the page
 		for link in soup.find_all('a', href=True):
 			url = link['href']
 
-			#formats URL
 			url = self.formatURL(base, url)
 			
 			#verifies link found is valid url and not a duplicate
 			if validators.url(url) and url not in self.URL_list and url not in self.visited and url != parent:
 				self.URL_list.append(url)
 
-	#finds and returns a random link from the list
 	def nextConnection(self):
 		if self.URL_list:
 			random = randrange(0, len(self.URL_list))
-			#if self.checkRbTXT(self.URL_list[random]):
-			return self.URL_list[random]
-			#else:
-			#	return "Excluded" #exclude those links that should not be crawled
+			#return self.URL_list[random]
+			#returns random url from page to follow
+			if self.checkRbTXT(self.URL_list[random]):
+				#print("Returning next available site to crawl")
+				return self.URL_list[random]
+			else:
+				return "Excluded"
 		else:
 			print("No more links to crawl")
 			return None
 
-	#removes invalid links from the list to avoid repeated attempts to crawl
 	def removeLink(self, url):
 		if url in self.URL_list:
 			self.URL_list.remove(url)
 
-	#performs the DFS on teh site
 	def search(self, ws, postid, database):
-		global endEarly
-		endEarly = False
-		_thread.start_new_thread(monitorSocket, (ws, postid, database))
-
-		#saves off information
 		currentURL = self.start
 		depth = 0
 		myParent = None
@@ -323,8 +295,8 @@ class DFS(Spider):
 		#while limit has not been reached, keyword hasn't been found and still urls available to crawl
 		while (len(self.visited) < self.limit+1) and not keywordFound and currentURL != None and not endEarly:
 
-			if self.checkMedia(currentURL):# and currentURL != "Excluded":
-				#currentURL.rstrip('/')
+			if self.checkMedia(currentURL) and currentURL != "Excluded":
+				currentURL.rstrip('/')
 
 				#parse that page
 				soup = self.parsePage(currentURL)
@@ -362,7 +334,7 @@ class DFS(Spider):
 					nextLink = self.nextConnection()
 					myParent = currentURL
 					currentURL = nextLink
-					depth += 1				#update depth
+					depth += 1
 
 					self.visited[currentURL] = True
 					
